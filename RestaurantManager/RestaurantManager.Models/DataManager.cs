c561ba2a01bf59e846f777e1cc4197dcdbccf089
fedorinoGore <fredy26@mail.ru>
2015-10-12 03:15:09 +0300

    Module 4 Homework
    Added some binding

---
+++ RestaurantManager/RestaurantManager.Models/DataManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace RestaurantManager.Models
+{
+    public class DataManager:DependencyObject //i guess?
+    {
+        public ObservableCollection<string> OrderItems { get; set; }
+        public List<string> MenuItems { get; set; }
+        public List<string> CurrentlySelectedMenuItems { get; set; }
+
+        public DataManager()
+        {
+            this.OrderItems = new ObservableCollection<string>(new List<string>{
+                "Steak, Chicken, Peas",
+                    "Rice, Chicken",
+                    "Hummus, Pita"
+            });
+            this.MenuItems = new List<string> {
+                "Steak",
+                "Chicken",
+                "Peas",
+                "Rice",
+                "Hummus",
+                "Pita"
+            };
+            this.CurrentlySelectedMenuItems = new List<string> {
+                "Rice",
+                "Pita"
+            };  
+        }
+    }
+}
